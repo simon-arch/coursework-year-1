70e1788d5a7cd31567667bdf4249c5acba137c22
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2025-02-05 20:24:03 +0200

    Refactored QuickBarEdit form

---
+++ filemanager/Dialogs/QuickBarEdit/Settings/QuickBarSettings.cs
@@ -0,0 +1,9 @@
+﻿using filemanager.Dialogs.QuickBarEdit.Models;
+
+namespace filemanager.Dialogs.QuickBarEdit.Settings;
+
+public record QuickBarSettings(
+    int ToolBarSize,
+    int ImageSize,
+    IList<QuickBarButton> Buttons
+);
